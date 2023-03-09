173fcf6dff89f8af58d351ccd7bb9826fced8d2a
deepika joshi <upreti.deepika@gmail.com>
2023-03-09 13:55:01 +0800

    updated list's migration file with null:false

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,5 @@
 class List < ApplicationRecord
   has_many :bookmars, dependent: :destroy
   has_many :movis, through: :bookmars, dependent: :destroy
-
-
   validates :name, uniqueness: true, presence: true
 end

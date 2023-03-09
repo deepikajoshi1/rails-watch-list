382908ac506154d2f25d8dca69d7e894600f2d75
deepika joshi <upreti.deepika@gmail.com>
2023-03-09 14:15:35 +0800

    updated bookmark's migration file with null:false

--- app/models/bookmar.rb
+++ app/models/bookmar.rb
@@ -2,6 +2,6 @@ class Bookmar < ApplicationRecord
   belongs_to :movi
   belongs_to :list
 
-  validates :comment, length: {minimum: 6}
-  validates :movi_id, uniqueness: {scope: :list_id, message: "is already in the list"}
+  validates :comment, length: { minimum: 6 }
+  validates :movi_id, uniqueness: { scope: :list_id, message: "is already in the list"}
 end
--- db/migrate/20221113172027_create_bookmars.rb
+++ db/migrate/20221113172027_create_bookmars.rb
@@ -1,9 +1,11 @@
 class CreateBookmars < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmars do |t|
-      t.string :comment
-      t.string :movie_id
-      t.string :list_id
+      t.string :comment, null: false
+      t.string :movie_id, null: false
+      t.string :list_id, null: false
+      t.references :movi, null: false, foreign_key: true
+      t.references :list, null: false, foreign_key: true
 
       t.timestamps
     end
